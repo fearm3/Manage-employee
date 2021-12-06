86153368db7a2b0355c1b5c6b6bd2fddef02583f
fearm3 <ibraa1628@gmail.com>
2021-12-06 18:50:29 +0300

    first commit

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import Employee from "./Employee";
+import { EmployeeContext } from "../contexts/EmployeeContext";
+const EmployeeList = () => {
+  const { employees } = useContext(EmployeeContext);
+
+  return (
+    <table className="table table-striped table-hover">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Address</th>
+          <th>Phone</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <Employee employees={employees} />
+      </tbody>
+    </table>
+  );
+};
+export default EmployeeList;
